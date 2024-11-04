8002bfac921d05107c5ac5ce3962f8bdd5310bd7
Josue Kaleb <josue.kalebyk@gmail.com>
2024-11-04 15:38:36 -0300

    Criei o arquivo teste em 04/11/2024

---
+++ Programs/src/main/java/prova/programs/Programs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prova.programs;
+
+/**
+ *
+ * @author Ycroowkkjk
+ */
+public class Programs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
